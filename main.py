d409c8df4a5e4359a95d303ce8f846b28c4a2b77
Vyacheslav Mitin <vyacheslav.mitin@gmail.com>
2021-11-03 03:07:39 +0400

    мелочи

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if os.getlogin() == ULYANOVSK:
 elif os.getlogin() == DIMITROVGRAD:
     CITY = 'dimitrovgrad'
 else:  # выход с ошибкой если не то имя логина в систему
-    sys.exit("Не подходящий логин в систему")
+    sys.exit("Не подходящий логин в систему!")
 
 ARCH_EXT = 'SevenZ'
 NOW_DATE = datetime.now().strftime('%d.%m.%Y')  # Текущая дата для работы с файлами и каталогами в формате 01.01.2021
@@ -46,15 +46,13 @@ elif CITY == 'dimitrovgrad':
 
 # Функции
 def make_dirs():
-    """Функция создания каталогов"""
-    print_log("Создание каталогов")
-
+    """Функция создания каталогов для работы модуля"""
     dirs = [TEMP_DIR, LOGS_DIR]
     [os.makedirs(os.path.join(SCRIPT_DIR, dir_), exist_ok=True) for dir_ in dirs]
 
 
 def logging():
-    """Функция логирования"""
+    """Функция логирования действий модуля"""
     pass  # TODO сделать логирование текстовым файлом
 
 
@@ -66,21 +64,20 @@ def cleaning_temp():
 
 
 def backuping():
-    """Функция резервного копирования баз 'ПО Участок инкассации'"""
-    cleaning_temp()  # очистка временной папки
+    """Функция выгрузки баз 'ПО Участок инкассации'"""
     print_log("Старт резервного копирования базы 'ПО Участок инкассации'")
 
     print_log("Работа SQL, начало:", line_before=True)
     subprocess.run([
         'sqlcmd', '-i', SQL_SCRIPT  # вызов программы со скриптом как параметр для выгрузки
-    ], timeout=600)  # , encoding='1251')
+    ], timeout=600)
     print_log("Работа SQL, конец", line_after=True)
 
     print_log("Окончание резервного копирования базы 'ПО Участок инкассации'")
 
 
 def compressing(base=''):
-    """Функция сжатия баз 'ПО Участок инкассации'"""
+    """Функция сжатия баз 'ПО Участок инкассации' архиватором"""
     print_log("Старт компрессии баз 'ПО Участок инкассации'")
 
     if CITY == 'ulyanovsk':
@@ -92,26 +89,26 @@ def compressing(base=''):
 
     print_log("Работа архиватора, начало:", line_before=True)
     for bases in glob.glob(f'POU_{base}_*.bak'):  # архивация с паролем
-        subprocess.run([
-            EXE_7Z,
+        subprocess.run([  # вызов архиватора 7zip с параметрами
+            EXE_7Z,  # путь к архиватору
             "a", "-t7z", "-m0=LZMA2:mt=6", "-mx=0", "-ssw",  # параметры для работы 7Zip
             f"POU_{base}_{NOW_DATE}_{NOW_WEEKDAY}_{NOW_TIME}.{ARCH_EXT}",  # итоговый файл
             bases,  # файл для архивирования
-            "-p" + PASS_7Z
+            "-p" + PASS_7Z  # пароль
         ], timeout=600)
     print_log("Работа архиватора, конец", line_after=True)
 
-    os.chdir(SCRIPT_DIR)  # возврат в папку скрипта
+    os.chdir(SCRIPT_DIR)  # возврат в папку модуля
 
     print_log("Окончание компрессии баз 'ПО Участок инкассации'")
 
 
 def moving_files():
-    """Функция перемещения баз"""
+    """Функция перемещения баз 'ПО Участок инкассации'"""
     print_log("Копирование и перемещение сжатых баз 'ПО Участок инкассации'")
 
     def working_with_archives(mode=''):
-        """Функция работы с файлами"""
+        """Функция работы с архивами баз"""
         archives_list = []  # создание листа с объектами - сжатыми базами
         [archives_list.append(archives) for archives in glob.glob(TEMP_DIR + '//' + f'*.{ARCH_EXT}')]
 
@@ -120,21 +117,21 @@ def moving_files():
         elif mode == 'move':  # перемещения в облако
             [shutil.move(archives, BACKUP_DIR_CLOUD) for archives in archives_list]
 
-    if CITY == 'ulyanovsk':
-        working_with_archives(mode='copy')
-        working_with_archives(mode='move')
+    if CITY == 'ulyanovsk':  # работа и с NAS и с облаком
+        working_with_archives(mode='copy')  # копия на NAS
+        working_with_archives(mode='move')  # перемещение в облако
     elif CITY == 'dimitrovgrad':  # только перемещения в облако
         working_with_archives(mode='move')
-
-    cleaning_temp()  # очистка временной папки
 
 
 if __name__ == '__main__':  # старт
     print_log("Начало работы скрипта по резервному копированию баз 'ПО Участок инкассации'",
               line_before=True, line_after=True)
-    make_dirs()  # создание каталогов
-    backuping()  # резервная копия SQL базы через SQLCMD в .bak файл
-    compressing()  # сжатие файла .bak в архив 7zip
-    moving_files()  # копирвоание и пермещение файлов
+    cleaning_temp()  # очистка временной папки
+    make_dirs()  # создание каталогов для работы модуля
+    backuping()  # резервная копия SQL базы через 'SQLCMD' в 'POU*.bak' файл
+    compressing()  # сжатие файла 'POU*.bak' в архив 7zip
+    moving_files()  # копирование и перемещение файлов
+    cleaning_temp()  # очистка временной папки
     print_log("Окончание работы скрипта по резервному копированию баз 'ПО Участок инкассации'",
               line_before=True, line_after=True)
